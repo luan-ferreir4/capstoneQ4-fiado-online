f39a8ec069674dcadf05e7a66bb95212c8d9c078
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-12 16:20:54 -0300

    feat: created inital migration

---
+++ src/migrations/1649791226574-initialCommit.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class initialCommit1649791226574 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
