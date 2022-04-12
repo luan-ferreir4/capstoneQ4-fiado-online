a503c0b19ab3bac587a5c36d511305119293cb6f
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-12 16:21:48 -0300

    refactor: formated initial migration structure

--- src/migrations/1649791226574-initialCommit.ts
+++ src/migrations/1649791226574-initialCommit.ts
@@ -1,11 +1,8 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+/* eslint-disable no-unused-vars */
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class initialCommit1649791226574 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
