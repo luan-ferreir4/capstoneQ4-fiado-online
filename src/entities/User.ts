9a51e1bd255fffa988c2ea267b2707b0b67beafb
Leonam Rodrigues <leonam_5000@hotmail.com>
2022-04-13 15:47:21 -0300

    Feat(entities/user): User entity created

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,37 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// import { Resources } from "./Resources"
 
-@Entity()
+@Entity('users')
 class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id_user: string;
 
   @Column()
-  firstName: string;
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  cpf: number;
 
   @Column()
-  lastName: string;
+  password: string;
 
   @Column()
-  age: number;
+  balance: number;
+
+  @Column({ type: 'date', default: () => 'NOW()' })
+  createdIn: Date;
+
+  @Column()
+  weekly_report_day: number;
+
+  @Column()
+  monthly_report_day: number;
+
+  // @OneToMany( type => Resources, resources => resources.user)
+  // resources: Resources[]
 }
 
 export default User;
