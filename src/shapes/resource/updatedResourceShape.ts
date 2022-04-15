732a9125ef6acc3eb4dae72caa3e836463b73f54
jerlyssonm <jerlyssonm@gmail.com>
2022-04-15 20:06:33 -0300

    feat: create shape for update infors

---
+++ src/shapes/resource/updatedResourceShape.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+const updatedResourceShape = yup.object().shape({
+  unit_cost: yup.number(),
+  units: yup.number(),
+  description: yup.string(),
+  updatedAt: yup.date().default(new Date()),
+});
+
+export default updatedResourceShape;
