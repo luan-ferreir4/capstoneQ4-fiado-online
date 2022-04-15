17a1eb60e020467637f88e391abd64394e1fbc0c
jerlyssonm <jerlyssonm@gmail.com>
2022-04-14 23:23:49 -0300

    fix: added import customers conttroler and exports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,15 @@
 import createUserController from './user/createUser';
+import createCustomController from './customer/createCustomer';
+import deleteCustomerController from './customer/deleteCustom';
+import getAllCustomersController from './customer/getAllCustomers';
+import updateCustomerController from './customer/updateCustomer';
+import getOneCustomerController from './customer/getOneCustomer';
 
-export { createUserController };
+export {
+  createUserController,
+  createCustomController,
+  deleteCustomerController,
+  getAllCustomersController,
+  updateCustomerController,
+  getOneCustomerController,
+};
