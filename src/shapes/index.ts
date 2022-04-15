dc534c0f87a179a80a29188b2d04ece628226d16
jerlyssonm <jerlyssonm@gmail.com>
2022-04-14 23:27:51 -0300

    fix: added imports Shapes and export shape custome

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -1,5 +1,13 @@
 import createUserShape from './user/createUserShape';
 import loginUserShape from './user/loginUserShape';
 import upgradeUserShape from './user/upgradeUserShape';
+import createCustomerShape from './customer/createCustomerShape';
+import updateCustomerShape from './customer/updateCustomerShape';
 
-export { createUserShape, loginUserShape, upgradeUserShape };
+export {
+  createUserShape,
+  loginUserShape,
+  upgradeUserShape,
+  createCustomerShape,
+  updateCustomerShape,
+};
