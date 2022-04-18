ed68a7daf91d76c3bb6fb5852052ffd25b2a3d82
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-18 12:24:47 -0300

    feat(services): export admin services

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+import updateAdminService from './admin/updateAdminService';
+import deleteAdminService from './admin/deleteAdminService';
+import registerAdminService from './admin/registerAdminService';
+import loginAdminService from './admin/loginAdminService';
+
+export {
+  updateAdminService,
+  deleteAdminService,
+  registerAdminService,
+  loginAdminService,
+};
