c8f73a2bea338f2e4c2debcbf0b7f56f9da53b4b
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-18 18:52:43 -0300

    feat: initialized sale services

--- src/controllers/sale/createSale.ts
+++ src/controllers/sale/createSale.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from 'express';
 import { Sale } from '../../entities';
-import { SaleRepository, ISale } from '../../repositories';
+import { ICreateSaleRequest, ISale } from '../../repositories';
+import { CreateSaleService, FormatSaleService } from '../../services';
 
 const createSaleController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const saleData: ISale = req.body;
+  const saleRequestData: ICreateSaleRequest = req.validated;
+  const { user } = req;
 
-  const newSale: Sale = new SaleRepository().createSale(saleData);
+  const { resources, ...saleData } = saleRequestData;
 
-  await new SaleRepository().saveSale(newSale);
+  // await new RegisterResourcesOnSale().execute(resources);
+
+  const newSaleData: ISale = await new FormatSaleService().execute(
+    saleData,
+    user.id_user
+  );
+
+  const newSale: Sale = await new CreateSaleService().execute(newSaleData);
 
   return res.json(201).json(newSale);
 };
