847ea7103a6bed115d8e5072d0aa5eedb66b6679
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-18 12:23:39 -0300

    feat(services/admin): add loginAdminService

---
+++ src/services/admin/loginAdminService.ts
@@ -0,0 +1,37 @@
+import { Request } from 'express';
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+
+import { AdminsRepository } from '../../repositories';
+import { ErrorHandler } from '../../utils';
+import { JWTConfig } from '../../configs';
+
+const loginAdminService = async (req: Request) => {
+  const { email, password } = req.body;
+
+  const admin = await new AdminsRepository().getOneAdmin(email);
+
+  if (!admin) {
+    throw new ErrorHandler(401, 'email or password is incorrect');
+  }
+
+  const match = bcrypt.compare(password, admin.password);
+
+  if (!match) {
+    throw new ErrorHandler(401, 'email or password is incorrect');
+  }
+
+  const token = jsonwebtoken.sign(
+    {
+      email: admin.email,
+      id_admin: admin.id_admin,
+      isAdm: admin.isAdm,
+    },
+    JWTConfig.secret,
+    { expiresIn: JWTConfig.expiresIn }
+  );
+
+  return token;
+};
+
+export default loginAdminService;
