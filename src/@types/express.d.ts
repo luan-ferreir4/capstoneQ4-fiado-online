255f77428084e9a89ee0f1b14e2230b1390a4f60
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-18 15:06:41 -0300

    feat: add admin to request

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,11 +1,12 @@
-import { Customer, User } from '../entities';
+import { Admin, Customer, User } from '../entities';
 
 declare global {
   namespace Express {
     interface Request {
-      validated: User | Customer;
+      validated: User | Customer | Admin;
       user: User;
       token: string;
+      admin: Admin;
     }
   }
 }
