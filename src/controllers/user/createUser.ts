769b74af49a6f6a4b7df77c9dc10f346762cb0b1
Leonam Rodrigues <leonam_5000@hotmail.com>
2022-04-18 14:56:57 -0300

    Att

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -6,15 +6,7 @@ const createUserController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const test = req.body;
-
-  console.log(req.body);
-
-  // const user: IUser = await new UserRepository().saveUser(
-  //   req.validated as IUser
-  // );
-
-  return res.status(201).json(test);
+  return res.status(201).json('');
 };
 
 export default createUserController;
---
+++ src/controllers/user/upgradeUser.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from 'express';
+
+const updateUserController = (req: Request, res: Response) => {};
+export default updateUserController;
