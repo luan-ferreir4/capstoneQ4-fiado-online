b9733c424c3b67faeddb0e110128c12fc7a23e04
Leonam Rodrigues <leonam_5000@hotmail.com>
2022-04-18 15:34:31 -0300

    Commit

--- src/controllers/user/deleteUser.ts
+++ src/controllers/user/deleteUser.ts
@@ -1,4 +1,11 @@
 import { Request, Response } from 'express';
+import { User } from '../../entities';
+import { UserRepository } from '../../repositories';
 
-const deleteUserController = (req: Request, res: Response) => {};
+const deleteUserController = async(req: Request, res: Response) => {
+  const authenticateUser = req.user;
+  await new UserRepository().deleteUser(authenticateUser['user'].id);
+
+  return res.status(204).json('');
+};
 export default deleteUserController;
