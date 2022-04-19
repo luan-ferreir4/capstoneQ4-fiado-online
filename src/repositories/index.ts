b7bcae4e3c21110ccbde509bbeaf5c5234efef18
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-18 23:45:30 -0300

    feat: ResorceSales repository and refactor sale interfaces

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,9 +1,19 @@
 import { UserRepository, IUser } from './user';
-import { SaleRepository, ISale, ICreateSaleRequest } from './sale';
+
+import { SaleRepository, ISale, IResourceRequest } from './sale';
+
 import { AdminsRepository, IAdmin, IUpdateAdmin } from './admin';
+
+import {
+  ResourceSaleRepository,
+  IResourceSale,
+  IResourceSaleRepo,
+} from './resourceSale';
 
 export { UserRepository, IUser };
 
 export { AdminsRepository, IAdmin, IUpdateAdmin };
 
-export { SaleRepository, ISale, ICreateSaleRequest };
+export { SaleRepository, ISale, IResourceRequest };
+
+export { ResourceSaleRepository, IResourceSale, IResourceSaleRepo };
