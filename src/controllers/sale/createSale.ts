882cd4681b983d94982b2bca7503bd803647be6d
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-18 23:47:40 -0300

    feat: created ResourceSale service

--- src/controllers/sale/createSale.ts
+++ src/controllers/sale/createSale.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 import { Sale } from '../../entities';
-import { ICreateSaleRequest, ISale } from '../../repositories';
-import { CreateSaleService, FormatSaleService } from '../../services';
+import { ISale } from '../../repositories';
+import {
+  RegisterResourcesOnSale,
+  CreateSaleService,
+  FormatSaleService,
+} from '../../services';
 
 const createSaleController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const saleRequestData: ICreateSaleRequest = req.validated;
+  const saleRequestData: ISale = req.validated;
   const { user } = req;
 
   const { resources, ...saleData } = saleRequestData;
-
-  // await new RegisterResourcesOnSale().execute(resources);
 
   const newSaleData: ISale = await new FormatSaleService().execute(
     saleData,
@@ -20,6 +22,8 @@ const createSaleController = async (
   );
 
   const newSale: Sale = await new CreateSaleService().execute(newSaleData);
+
+  await new RegisterResourcesOnSale().execute(resources, newSale.id_sale);
 
   return res.json(201).json(newSale);
 };
