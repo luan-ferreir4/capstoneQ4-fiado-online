f12a608f487c8ceda6686c97b7cb348d17580e2f
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-20 16:09:02 -0300

    refactor: resorce sales criation

--- src/services/resourceSale/registerResourcesOnSale.ts
+++ src/services/resourceSale/registerResourcesOnSale.ts
@@ -14,27 +14,24 @@ class RegisterResourcesOnSale {
   async execute(requestData: IResourceRequest[], id_sale: string) {
     try {
       const resourceSaleRepository = new ResourceSaleRepository();
+      const resourceRepository = new ResourceRepository();
 
-      // const resourceSaleList: Promise<IResourceSale[]> = requestData.map(
-      //   async (item) => {
-      //     // pega recursos pelo nome que são uma chave unica
-      //     const resource = await new ResourceRepository().getResourceByName(
-      //       item.resource_name
-      //     );
-      //     const resourceSaleData: IResourceSale = {
-      //       // pega o id_sale após ter a sale criada com o service no controller
-      //       id_sale,
-      //       // pegar id_sale após ter a criado com o service no controller
-      //       id_resource: resource.id_resource,
-      //       quantity: item.quantity,
-      //     };
-      //     return resourceSaleData;
-        }
-      );
+      const resourceSaleList: ResourceSale[] = requestData.map(async (item) => {
+        const resource = await resourceRepository.getResourceByName(
+          item.resource_name
+        );
+        const resourceSaleData: IResourceSale = {
+          id_sale, // pegar id_sale após ter a criado com o service no controller
+          id_resource: resource.id_resource, // pegar recursos pelo nome que será uma chave unica
+          quantity: item.quantity,
+        };
+        return resourceSaleData;
+      });
 
       resourceSaleList.forEach(async (item) => {
-        const newSale: ResourceSale = resourceSaleRepository.createSale(item);
-        await resourceSaleRepository.saveSale(newSale);
+        const newSale: ResourceSale =
+          resourceSaleRepository.createResourceSale(item);
+        await resourceSaleRepository.saveResourceSale(newSale);
       });
     } catch (error) {
       const { detail } = error;
