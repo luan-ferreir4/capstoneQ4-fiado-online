7cf8acd8ccbdd87e7ad9f5cb3aa3f2f8ecc1f348
jerlyssonm <jerlyssonm@gmail.com>
2022-04-20 20:05:44 -0300

    fix: added in customerShape regex for phone number

--- src/shapes/customer/updateCustomerShape.ts
+++ src/shapes/customer/updateCustomerShape.ts
@@ -3,7 +3,12 @@ import * as yup from 'yup';
 const updateCustomerShape = yup.object().shape({
   name: yup.string(),
   email: yup.string().email().lowercase(),
-  phone_number: yup.string(),
+  phone_number: yup
+    .string()
+    .matches(
+      /[0-9]{2}-9[0-9]{4}-[0-9]{4}/,
+      'Format number: 00-90000-0000. ps: the third digit must equal 9'
+    ),
 });
 
 export default updateCustomerShape;
