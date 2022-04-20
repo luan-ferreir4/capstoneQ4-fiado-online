3d0c766d88dd98a1ce6d4a0cedaef693268811a4
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-20 14:20:52 -0300

    feat(middlewares): create admin auth middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,5 +2,6 @@ import validateShape from './validateShapeMiddleware';
 import checkLogin from './checkLoginMiddleware';
 import authUser from './authUserMiddleware';
 import verifyExistingSale from './verifyExistingSaleMiddleware';
+import authAdmin from './authAdminMiddleware';
 
-export { validateShape, verifyExistingSale, checkLogin, authUser };
+export { validateShape, verifyExistingSale, checkLogin, authUser, authAdmin };
