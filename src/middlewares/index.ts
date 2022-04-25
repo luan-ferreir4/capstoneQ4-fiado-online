ebc5d036d65bcf1653d210601798cacfb0715f62
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-04-25 11:31:09 -0300

    refactor: middlewares folders structure

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,19 +1,19 @@
-import validateShape from './validateShapeMiddleware';
-import checkLogin from './checkLoginMiddleware';
-import authUser from './authUserMiddleware';
-import verifyExistingSale from './verifyExistingSaleMiddleware';
-import authAdmin from './authAdminMiddleware';
-import verifyEmailAlreadyExists from './verifyEmailAlreadyExists';
-import verifyExistingResourceMiddleware from './verifyExistingResourceMiddleware';
-import verifyIdResourceExistsMiddleware from './verifyIdResourceExistsMiddleware';
+import authAdmin from './jwt/authAdminMiddleware';
+import authUser from './jwt/authUserMiddleware';
+import checkLogin from './jwt/checkLoginMiddleware';
+import verifyIdResourceExistsMiddleware from './resource/verifyIdResourceExistsMiddleware';
+import verifyExistingResourceMiddleware from './resource/verifyExistingResourceMiddleware';
+import verifyExistingSale from './sale/verifyExistingSaleMiddleware';
+import verifyEmailAlreadyExists from './user/verifyEmailAlreadyExists';
+import validateShape from './yup/validateShapeMiddleware';
 
 export {
-  validateShape,
-  verifyExistingSale,
-  checkLogin,
   authUser,
   authAdmin,
-  verifyEmailAlreadyExists,
+  checkLogin,
   verifyExistingResourceMiddleware,
   verifyIdResourceExistsMiddleware,
+  verifyExistingSale,
+  verifyEmailAlreadyExists,
+  validateShape,
 };
