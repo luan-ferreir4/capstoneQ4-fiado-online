0082ce0f9150747a56344aca0c67259dc672409f
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-27 10:52:49 -0300

    feat(shapes): add hash to password in validation shape

--- src/shapes/admin/createAdminShape.ts
+++ src/shapes/admin/createAdminShape.ts
@@ -1,9 +1,14 @@
 import * as yup from 'yup';
 
+import { hashSync } from 'bcrypt';
+
 const createAdmin = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().lowercase().required(),
-  password: yup.string().required(),
+  password: yup
+    .string()
+    .transform((pwd) => hashSync(pwd, 10))
+    .required(),
   isAdm: yup.bool().default(true),
 });
 
