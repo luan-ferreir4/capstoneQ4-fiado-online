e92aa532230a2393be0254f660d1edbcb8ea241b
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-04-28 14:04:49 -0300

    feat(routes): change user routes for admins to admins routes

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -7,7 +7,12 @@ import {
   updateAdminController,
   deleteAdminController,
   getAdminProfileController,
+  updateUserByIdController,
+  getAllUsersController,
+  deleteUserByIdController,
 } from '../../controllers';
+
+import getUserProfileByIdController from '../../controllers/user/getUserProfileById';
 
 import {
   validateShape,
@@ -15,9 +20,16 @@ import {
   verifyResquestBodyToUpdateMiddleware,
 } from '../../middlewares';
 
-import { createAdmin, loginAdmin, updateAdmin } from '../../shapes';
+import {
+  createAdmin,
+  loginAdmin,
+  updateAdmin,
+  upgradeUserShape,
+} from '../../shapes';
 
 const adminsRouter = Router();
+
+//  Admin Routes
 
 adminsRouter.get('', authAdmin, getAdminsController);
 
@@ -40,5 +52,25 @@ adminsRouter.patch(
 );
 
 adminsRouter.delete('/:id_admin', authAdmin, deleteAdminController);
+
+//  Admin Routes
+
+//  Admin User Routes
+
+adminsRouter.get('/user/all', authAdmin, getAllUsersController);
+
+adminsRouter.get('/user/:id_user', authAdmin, getUserProfileByIdController);
+
+adminsRouter.patch(
+  '/user/:id_user',
+  authAdmin,
+  verifyResquestBodyToUpdateMiddleware('user'),
+  validateShape(upgradeUserShape),
+  updateUserByIdController
+);
+
+adminsRouter.delete('/user/:id_user', authAdmin, deleteUserByIdController);
+
+//  Admin User Routes
 
 export default adminsRouter;
